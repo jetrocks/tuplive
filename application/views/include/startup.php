17f9ee6e15438f0ffddafaaf4e3c0b94a3be234e
PtmDex <31686458+PtmDex@users.noreply.github.com>
2017-10-13 15:58:21 +0800

    Create startup.php

---
+++ application/views/include/startup.php
@@ -0,0 +1,48 @@
+<!DOCTYPE html>
+<html lang="en-US">
+   <head>
+      <meta charset="UTF-8">
+	  
+      <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+      <title>TUP Live!</title>
+      <link rel="stylesheet" href="<?php echo base_url('bootstrap/css/components.css');?>">
+      <link rel="stylesheet" href="<?php echo base_url('bootstrap/css/icons.css');?>">
+	  <link rel="stylesheet" href="<?php echo base_url('bootstrap/css/style.css');?>">
+      <link rel="stylesheet" href="<?php echo base_url('bootstrap/css/responsee.css');?>">
+      <link href='http://fonts.googleapis.com/css?family=Open+Sans:400,300,600,700,800&subset=latin,latin-ext' rel='stylesheet' type='text/css'>
+      <script type="text/javascript" src="<?php echo base_url('bootstrap/js/jquery-1.8.3.min.js');?>"></script>
+      <script type="text/javascript" src="<?php echo base_url('bootstrap/js/jquery-ui.min.js');?>"></script>    
+	  
+
+   </head>
+   <style>
+		body {
+    margin: 0;
+    font-family: 'Lato', sans-serif;
+	background-image: url("TLimages/bg.jpg");
+	}
+	
+	
+   </style>
+   <body class="size-960">
+   
+	
+		
+	<div class="container-fluid">
+		<div>
+			
+		</div>
+	</div>
+                           
+
+	
+	
+      <script type="text/javascript" src="<?php echo base_url('bootstrap/js/responsee.js');?>"></script>               
+      
+	  <script src='http://cdnjs.cloudflare.com/ajax/libs/jquery/2.1.3/jquery.min.js'></script>
+
+    <script  src="<?php echo base_url('bootstrap/js/index.js');?>"></script>
+	
+	
+   </body>
+</html>
