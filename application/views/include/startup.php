a20971cc8efae6bd7e567b262dc74b6451167385
PtmDex <31686458+PtmDex@users.noreply.github.com>
2017-10-16 13:07:42 +0800

    Update startup.php

--- application/views/include/startup.php
+++ application/views/include/startup.php
@@ -6,6 +6,7 @@
       <meta name="viewport" content="width=device-width, initial-scale=1.0" />
       <title>TUP Live!</title>
       <link rel="stylesheet" href="<?php echo base_url('bootstrap/css/components.css');?>">
+	<link rel="stylesheet" href="<?php echo base_url('bootstrap/css/bootstrap.css');?>">
       <link rel="stylesheet" href="<?php echo base_url('bootstrap/css/icons.css');?>">
 	  <link rel="stylesheet" href="<?php echo base_url('bootstrap/css/style.css');?>">
       <link rel="stylesheet" href="<?php echo base_url('bootstrap/css/responsee.css');?>">
