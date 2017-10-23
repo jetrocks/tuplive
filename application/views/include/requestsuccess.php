b5f778669089c64670c33f9f2665c0d29da1941d
Angel Villoria <30525269+angelvilloria@users.noreply.github.com>
2017-10-23 11:22:48 +0800

    Add files via upload

--- application/views/include/requestsuccess.php
+++ application/views/include/requestsuccess.php
@@ -3,7 +3,7 @@
 <head>
 	<title>TUP Live!</title>
 	<meta name="viewport" content="width=device-width, initial-scale=1.0" />
-	<meta charset="UTF-8"  />
+	<meta charset="UTF-8" />
 	<link href="<?php echo base_url('bootstrap/css/bootstrap.css'); ?>" rel="stylesheet" />
 	<link href="<?php echo base_url('bootstrap/css/bootstrap-theme.css'); ?>" rel="stylesheet" />
 	<link href="<?php echo base_url('bootstrap/css/custom.css'); ?>" rel="stylesheet" />
@@ -11,6 +11,7 @@
 	<link href="<?php echo base_url('bootstrap/css/icons.css'); ?>" rel="stylesheet" />
 	<link href="<?php echo base_url('bootstrap/css/style.css'); ?>" rel="stylesheet" />
 	<link href="<?php echo base_url('bootstrap/css/response.css'); ?>" rel="stylesheet" />
+	<link href="<?php echo base_url('bootstrap/css/watchlist.css'); ?>" rel="stylesheet" />
 	<link rel="shortcut icon" href="<?php echo base_url('bootstrap/css/tup.ico'); ?>">
 	<link href='http://fonts.googleapis.com/css?family=Open+Sans:400,300,600,700,800&subset=latin,latin-ext' rel='stylesheet' type='text/css'>
 	<link rel="shortcut icon" href="../TupLive/TLimages/tup.ico">
@@ -20,6 +21,7 @@
 	<script src="<?php echo base_url('bootstrap/js/jquery-ui.min.js');?> "></script>
 	<script src="<?php echo base_url('bootstrap/js/responsee.js');?> "></script>
 	<script src="<?php echo base_url('bootstrap/js/index.js');?> "></script>
+	<script src="<?php echo base_url('bootstrap/js/watchlist.js');?> "></script>
 	<script src='http://cdnjs.cloudflare.com/ajax/libs/jquery/2.1.3/jquery.min.js'></script>
 	<script type="text/javascript">
          jQuery(document).ready(function($) {	  
@@ -40,10 +42,65 @@
          });	 
       </script>
 	
+	
+	<script>
+		
+			
+		checks=0;
+		function showDiv(elem){
+			if(elem.value == "Weekly"){
+				$("#day").slideDown('medium');
+				$("#dayy").slideDown('medium');
+			}
+			if(elem.value == "Daily"){
+				$("#dayy").slideDown('medium');
+				$("#day").slideUp('medium');
+			}
+			if(elem.value == "Never") {
+				$("#day").slideUp('medium');
+				$("#dayy").slideUp('medium');
+			}
+		}
+		
+		function adding(){
+			if(document.getElementById("day1").checked == true &&
+				document.getElementById("day2").checked == true &&
+				document.getElementById("day3").checked == true &&
+				document.getElementById("day4").checked == true &&
+				document.getElementById("day5").checked == true &&
+				document.getElementById("day6").checked == true &&
+				document.getElementById("day7").checked == true){
+				
+				$("#day").slideUp('medium');
+			
+				document.getElementById("day1").checked = false;
+				document.getElementById("day2").checked = false;
+				document.getElementById("day3").checked = false;
+				document.getElementById("day4").checked = false;
+				document.getElementById("day5").checked = false;
+				document.getElementById("day6").checked = false;
+				document.getElementById("day7").checked = false;
+				document.getElementById("selpeat").selectedIndex = 1;
+			}
+		}
+	</script>
 
 
 <style>
-	
+	body{
+		background: url(TLimages/bg.jpg);
+		 no-repeat center center fixed; 
+		 font-family:"Montserrat";
+		 text-align:left;
+	-webkit-background-size: cover;
+	-moz-background-size: cover;
+	-o-background-size: cover;
+	background-size: cover;
+	position: relative;  
+	color: #ffffff;
+	overflow-x: hidden;
+		
+	}
 	.botroundy{
 		border-radius: 0 0 25px 25px;
 	}
@@ -55,20 +112,7 @@
 		width: 200px;
 		height: 50px;
 	}
-	body{
-		background-color: #1a1a1a;
-		background-image: url('TLimages/bg.jpg'); 
-		font-family:"Montserrat";
-		 no-repeat center center fixed; 
-		-webkit-background-size: cover;
-		-moz-background-size: cover;
-		-o-background-size: cover;
-		background-size: cover;
-		position: relative;  
-		color: #ffffff;
-		overflow-x: hidden;
-		
-	}
+	
 	.headcolor{
 		background-color: #0d0d0d;
 		box-shadow: 0px 10px 5px #1a1a1a;
@@ -76,42 +120,19 @@
 		
 	}
 	.headcolorroundy{
-		background-color: #0d0d0d;
+		background-image: url('TLimages/facade.png');
+		-webkit-background-size: cover;
+	-moz-background-size: cover;
+	-o-background-size: cover;
+	background-size: cover;
+	position: relative;  
+	color: #ffffff;
+	overflow-x: hidden;
 		box-shadow: 0px 10px 5px #1a1a1a;
 		border-radius: 25px 25px 25px 25px;
 		
+		
 	}
-	.table-curved {
-    border-collapse: separate;
-}
-.table-curved {
-    border: solid #ccc 1px;
-    border-radius: 6px;
-    border-left:0px;
-}
-.table-curved td, .table-curved th {
-    border-left: 1px solid #ccc;
-    border-top: 1px solid #ccc;
-}
-.table-curved th {
-    border-top: none;
-}
-.table-curved th:first-child {
-    border-radius: 6px 0 0 0;
-}
-.table-curved th:last-child {
-    border-radius: 0 6px 0 0;
-}
-.table-curved th:only-child{
-    border-radius: 6px 6px 0 0;
-}
-.table-curved tr:last-child td:first-child {
-    border-radius: 0 0 0 6px;
-}
-.table-curved tr:last-child td:last-child {
-    border-radius: 0 0 6px 0;
-}
-
 	.topp{
 		z-index: 1;
 	}
@@ -191,17 +212,50 @@
 		width: 40px;
 		display:inline-block;
 	}
-	table {
-    width: 50%;
-    margin-left: auto;
-    margin-right: auto;
-}
+	.boxkular{
+		background-color: #c51d39;
+		border-radius: 15px;
+		
+	}
+
+	.boxkular form{
+		color: #white;
+		font-size: 10px;
+	}
+	.boxkular button, form button{
+		font-size: 20px;
+	}
+	.sidetext h1{
+		color: white;
+		font-size: 50px;
+		font-family: bookman;
+	}
+	.sidetext h2{
+		color: white;
+		font-size: 30px;
+		font-family: bookman;
+	}
+	.formy{
+		color: white;
+	}
+	select{
+		color: black;
+		font-size: 10px;
+		padding: 10px;
+		border-radius: 5px;
+	}
+	.checkk input[type=checkbox]{
+		
+	}
+	.align{
+		text-align: left;
+	}
 	.navvy li{
 	font-size: 15px;
 }
 </style>
 </head>
-<body class="font" >
+<body class="align">
 	<div class="container">
 	<br />
 	<!-- header -->	
@@ -212,7 +266,7 @@
 					<img class="logo" src="<?php echo base_url('TLimages/tupweb.png'); ?>" alt="logo">
 				</div>
 				
-				<div class="s-8 m-8 l-4" >
+				<div class="s-8 m-8 l-4 align" >
 					<img class="logo2" src="<?php echo base_url('TLimages/livetitle.png'); ?>" alt="title">
 					<h4>The Technological University of the Philippines'<br />Live Streaming Broadcasting Website</h4>
 				</div>
@@ -234,8 +288,8 @@
 			<div class="collapse navbar-collapse" id="myNavbar">
 			<ul class="nav navbar-nav navvy" >
 				<li><a href="<?php echo base_url(); ?>">Now Streaming</a></li>
-				<li class="active"><a href="<?php echo base_url('watchlist'); ?>">Watchlist</a></li>
-				<li><a href="<?php echo base_url('request'); ?>">Request</a></li>
+				<li><a href="<?php echo base_url('watchlist'); ?>">Watchlist</a></li>
+				<li class="active"><a href="<?php echo base_url('request'); ?>">Request</a></li>
 				<li><a href="<?php echo base_url('about'); ?>">About Us</a></li>
 			</ul>
 			</div>
@@ -245,65 +299,34 @@
 	</header>
 
 	<section>
-		<div class="box container2 headcolorroundy">
-			<div>
-				Today's Schedule (<?php echo $date; ?>)
-			</div>
-			<div>
-				<div >
+		<div class="box container2 headcolorroundy ">
+			<div class="line ">
+				<div  class="s-12 m-12 l-6 sidetext align">
+					<h1  style="font-family:Montserrat; margin-left:50px;" >
+							Technological University of the Philippines is live now!
+					</h1>
 					<br />
-					 <table class="s-6 m-6 l-6 table table-hover table-curved" style="color: #0d0d0d; text-align: center; width:100%; ">
-						<thead style="text-align: center;">
-							<tr>
-							<th >TIME</th>
-							<th>PROGRAM</th>
-							</tr>
-						</thead>
-						<tbody>
-						<?php
-						$date_today= date("Y-m-d", strtotime($date));
-						$day_today=date("l", strtotime($date));
-						if(isset($prog) && isset($rep)){
-							foreach ($prog as $p){
-								if(($p['rep'] == 'Daily' && date("Y-m-d",strtotime($p['startdate'])) >= $date_today || date("Y-m-d",strtotime($p['enddate'])) <= $date_today )) {
-										echo '<tr>';
-										echo '<td>'.date("h:ia",strtotime($p['starttime'])).' - '.date("h:ia",strtotime($p['endtime'])).'</td>';
-										echo '<td>'.$p['progname'].'</td>';
-										echo '</tr>';
-										}
-								else if(($p['rep'] == 'Weekly' && date("Y-m-d",strtotime($p['startdate'])) >= $date_today || date("Y-m-d",strtotime($p['enddate'])) <= $date_today )){
-									foreach ($rep as $pr){
-										if($p['prog_id'] == $p['prog_id'] && $pr['repeat_every'] == $day_today){
-											echo '<tr>';
-											echo '<td>'.date("h:ia",strtotime($p['starttime'])).' - '.date("h:ia",strtotime($p['endtime'])).'</td>';
-											echo '<td>'.$p['progname'].'</td>';
-											echo '</tr>';
-											}
-
-										}
-						
-										}
-								else if(($p['rep'] == 'Never' && date("Y-m-d",strtotime($p['startdate'])) == $date_today)){
-										echo '<tr>';
-										echo '<td>'.date("h:ia",strtotime($p['starttime'])).' - '.date("h:ia",strtotime($p['endtime'])).'</td>';
-										echo '<td>'.$p['progname'].'</td>';
-										echo '</tr>';
-								}		
-
-									}
-								}		
-								
-							?>	
-							
-
-						</tbody>
-					</table>
+					<h2  style="font-family:Montserrat;  margin-left:50px;" >
+							This is TUP's one and powerful Live Streaming Website.<br />
+							Take a peek to the small but powerful world of TUP.<br />
+							Feel at home in your second-home by reaching out your laptop or smartphone. 
+					</h2>
 				</div>
+				
+				<div id="log" class="box s-12 m-10 l-5 boxkular" style="margin:40px; top:50px;">
+					<br />
+					<h1 style="color: white;">Request for a Timeslot </h1>
+					<h4 style="color: white;">and shine bright. </h4>
+					<br />
+					<br />
+					<h1 style="color: white;">Success! Please <a href="https://mail.google.com/" >open your gmail account</a> to receive the link for the form! </h1>
+				</div>	
 			</div>
-			
 		</div>
-		<br />	
-		</section>
+	</section>
+	
+	<br />
+	
 	<footer class="align" >
 		<div class="box" style="width:100%">
 			<div class="line"><br />
@@ -341,6 +364,7 @@
 		</div>
 
 		</footer>
+	
 	</div>
 </body>
 </html>
