1f3dc11243f181f321512aeff22018d6d67a4c70
PtmDex <31686458+PtmDex@users.noreply.github.com>
2017-10-23 10:43:41 +0800

    asset files mailer

---
+++ assets/success.php
@@ -0,0 +1,33 @@
+<?php
+require 'PHPMailerAutoload.php';
+
+$mail = new PHPMailer;
+
+//$mail->SMTPDebug = 3;                               // Enable verbose debug output
+
+$mail->isSMTP();                                      // Set mailer to use SMTP
+$mail->Host = 'smtp.gmail.com';  // Specify main and backup SMTP servers
+$mail->SMTPAuth = true;                               // Enable SMTP authentication
+$mail->Username = 'dexfornillos3@gmail.com';                 // SMTP username
+$mail->Password = 'linapo123';                           // SMTP password
+$mail->SMTPSecure = 'ssl';                            // Enable TLS encryption, `ssl` also accepted
+$mail->Port = 465;                                    // TCP port to connect to
+
+$mail->setFrom('dexfornillos3@gmail.com', 'Dexxy');
+$mail->addAddress('jekjekfornillos@gmail.com', 'Dex Fornillos');     // Add a recipient
+
+
+$mail->isHTML(true);                                  // Set email format to HTML
+
+$mail->Subject = 'Thank you!';
+$mail->Body    = 'Your schedule request has been submitted! Please blah blah blah';
+$mail->AltBody = 'This is the body in plain text for non-HTML mail clients';
+
+if(!$mail->send()) {
+    echo 'Message could not be sent.';
+    echo 'Mailer Error: ' . $mail->ErrorInfo;
+} else {
+    echo 'Message has been sent';
+	$newURL = base_url('admin');
+	header('Location: '.$newURL);
+}
